fa61e2558067dec28f83b20c144670447b5fdee4
helori <helori.lanos@gmail.com>
2017-09-08 11:26:38 +0200

    Last commit forgotten files ...

--- src/views/elements.blade.php
+++ src/views/elements.blade.php
@@ -6,7 +6,9 @@
 	<element-manager
 		uri-prefix="/admin"
 		fieldset-id="{{ $fieldset->id }}"
-		editor-css="{{ mix('css/tinymce.css') }}">
+		editor-css="{{ mix('css/tinymce.css') }}"
+		:editor-options="{{ json_encode(config('laravel-cms.editorOptions')) }}"
+		editor-assets-url="/admin/api/media?limit=0">
 	</element-manager>
 
 @else
@@ -14,7 +16,9 @@
 	<elements-manager
 		uri-prefix="/admin"
 		fieldset-id="{{ $fieldset->id }}"
-		editor-css="{{ mix('css/tinymce.css') }}">
+		editor-css="{{ mix('css/tinymce.css') }}"
+		:editor-options="{{ json_encode(config('laravel-cms.editorOptions')) }}"
+		editor-assets-url="/admin/api/media?limit=0">
 	</elements-manager>
 
 @endif
